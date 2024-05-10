4f3c28eb89669c12a659e6dfebdfd3be3b681396
Suporte <suporte@AGUATIVA.LOCAL>
2024-05-10 15:23:44 -0300

    feat: add router categoria

--- src/router/categoria-router.ts
+++ src/router/categoria-router.ts
@@ -1,6 +1,13 @@
 import { Hono } from "hono";
+import auth from "../middleware/auth";
 
 const categoriaRouter = new Hono();
 
+categoriaRouter.get('/',auth,(c) => {
+
+    console.log(c.get('jwtPayload'))
+
+    return c.json({})
+})
 
 export default categoriaRouter;
