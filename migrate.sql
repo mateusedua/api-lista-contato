971b4fd40b3e44c4372ee174610d9c25f46997aa
Mateus Eduardo <45969051+mateusedua@users.noreply.github.com>
2024-05-12 14:35:19 -0300

    feat:add more inserts of categoria

--- migrate.sql
+++ migrate.sql
@@ -1,6 +1,6 @@
-drop table categoria;
-drop table usuario;
-drop table contato;
+drop table if EXISTS contato;
+drop table if EXISTS categoria;
+drop table if EXISTS usuario;
 
 create table categoria(
 	id_categoria varchar(240) PRIMARY key,
@@ -29,7 +29,9 @@ create table contato(
 );
 
 insert into categoria(id_categoria, categoria)
-values('2e6f9b0d5885b6010f9167787445617f553a735f', 'Particular');
+values('2e6f9b0d5885b6010f9167787445617f553a735f', 'Particular'),
+('aad5f3e1f7d8fc48378d89fc4483bb84','Trabalho'),
+('7fd3316897b06f6c7ad6888e1f3cdec8','Instagram');
 
 insert into usuario(id_usuario, nome, email, password)
 values('09f02ace9d36ad7a583e4fb252fb957e', 'Mateus Eduardo', 'mateus@gmail.com', '123');
